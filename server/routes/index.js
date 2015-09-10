8675d8c2414d4ca1b5f50409e81a3ab040a3f7af
reydelo <reyna.deloge@gmail.com>
2015-09-10 09:28:34 -0600

    create html form and table for CRUD kittens

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,7 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'CRUD Kittens' });
 });
+
+router.post('/', function(req, res) {
+  console.log(req.body);
+  res.render('index', {title: 'CRUD Kittens'});
+})
 
 module.exports = router;
